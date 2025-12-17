2e6063b44888640ed0259b0bf04773b332337c5b
Braco <braco@MacBook-Pro-de-Braco.local>
2025-12-17 13:35:19 -0500

    api axios

--- backend/main.py
+++ backend/main.py
@@ -73,6 +73,7 @@ app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY, same_site=
 
 allowed_origins = [
     "https://tax.capbraco.com",
+    "https://api.capbraco.com",
     "https://www.tax.capbraco.com",
 
     #local dev
